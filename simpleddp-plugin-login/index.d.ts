5aa06b03c522d47c14a8c44b24b2b78dbe48354f
Nick Aquina <nickaquina@gmail.com>
2020-06-27 20:05:52 +0200

    Types for simpleddp-plugin-login

---
+++ simpleddp-plugin-login/index.d.ts
@@ -0,0 +1,6 @@
+declare module "simpleddp-plugin-login"
+// Types of functions provided by the login plugin are added to the types of
+// simpleddp, we thus assume that this plugin is installed.
+// todo: find a way to only show the types of the functions provided by the
+// login plugin when it is installed.
+// todo: types for opts
